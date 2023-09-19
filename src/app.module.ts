37f36af87d42250d927d42aa0a313f978d192948
Vasantha-idexcel <vasantha.r@idexcel.com>
2023-09-19 14:35:29 +0530

    Added Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule, providePrismaClientExceptionFilter } from 'nestjs-prisma';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule.forRoot()],
   controllers: [],
-  providers: [],
+  providers: [
+    // register filter here or in main.ts
+    // {
+    //   provide: APP_FILTER,
+    //   useFactory: ({ httpAdapter }: HttpAdapterHost) => {
+    //     return new PrismaClientExceptionFilter(httpAdapter);
+    //   },
+    //   inject: [HttpAdapterHost],
+    // },
+    // or
+    providePrismaClientExceptionFilter()
+  ],
 })
 export class AppModule {}
